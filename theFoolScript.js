284e8ca0223fa5fa221d81318e38c47dc7ac359b
Bionicobot <bionicobot2@gmail.com>
2019-03-06 14:35:53 -0600

    Made animation

--- theFoolScript.js
+++ theFoolScript.js
@@ -4,11 +4,17 @@ var animate = window.requestAnimationFrame ||
     function(callBack) { window.setTimeout(callBack, 1000/60)};
 
 var canvas = document.getElementById('canvas');
-var width = 480;
-var height = 480;
+var width = 240;
+var height = 240;
 canvas.width = width;
 canvas.height = height;
 var context = canvas.getContext('2d');
+var frameCount = 0;
+var frame = 0;
+var dir = 0;
+
+var MyIm = new Image();
+MyIm.src = "fool.png";
 
 window.onload = function() {
   document.body.appendChild(canvas);
@@ -22,11 +28,21 @@ var step = function(){
 };
 
 var update = function() {
+    if(++frameCount >= 10){
+        frameCount = 0;
+    if(++frame >= 4){
+        frame = 0;
+       if(++dir >= 4){
+          dir = 0;
+          }
+       }
+    }
 };
 
 var render = function(){
     context.fillStyle = '#17111A';
   context.fillRect(0,0,width,height);
+    context.drawImage(MyIm, 0 + (16 * frame), 0 + (16 * dir), 16, 16, 0, 0, 16, 16);
     
 };
 var npcAct = [ npc000 ];
