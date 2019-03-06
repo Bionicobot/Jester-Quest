774ffe1ab6dea155fcf5cb209323c13b4adb3747
JohannesBa <48295866+JohannesBa@users.noreply.github.com>
2019-03-06 13:54:23 -0600

    blah blah

---
+++ theFoolScript.js
@@ -0,0 +1,43 @@
+var animate = window.requestAnimationFrame || 
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function(callBack) { window.setTimeout(callBack, 1000/60)};
+
+var canvas = document.createElement('canvas');
+var width = 480;
+var height = 480;
+canvas.width = width;
+canvas.height = height;
+var context = canvas.getContext('2d');
+
+window.onload = function() {
+  document.body.appendChild(canvas);
+  animate(step);
+};
+
+var step = function(){
+    update();
+    render();
+    animate(step);
+};
+
+var update = function() {
+};
+
+var render = function(){
+    context.fillStyle = '#17111A';
+  context.fillRect(0,0,width,height);
+    
+};
+var npcAct = [ npc000 ];
+
+function NPC( npcType, x,  y,  xm,  ym,  dir,  state,  parent){
+    this.type = npcType;
+    this.x = x; 
+    this.y = y;
+    this.xm = xm;
+    this.ym = ym;
+    this.dir = dir;
+    this.state = state;
+    this.parent = parent||false;
+}
