b3f2824203f5e974076d9190c5288b5fe0d406ff
Bionicobot <bionicobot2@gmail.com>
2019-03-07 14:05:41 -0600

    Update theFoolScript.js

--- theFoolScript.js
+++ theFoolScript.js
@@ -12,6 +12,7 @@ var context = canvas.getContext('2d');
 var frameCount = 0;
 var frame = 0;
 var dir = 0;
+var gameState = 0;
 
 var MyIm = new Image();
 MyIm.src = "fool.png";
@@ -21,9 +22,52 @@ window.onload = function() {
   animate(step);
 };
 
+var KVAL = {
+    UP      : 0b1,
+    DOWN    : 0b10,
+    LEFT    : 0b100,
+    RIGHT   : 0b1000,
+    Z       : 0b10000,
+    X       : 0b100000,
+    C       : 0b1000000,
+    ESC     : 0b10000000,
+    SHIFT   : 0b100000000,
+};
+
+var keysDown = 0;
+
+window.addEventListener("keydown", function(event) {
+  keysDown[event.keyCode] = true;
+});
+
+window.addEventListener("keyup", function(event) {
+  delete keysDown[event.keyCode];
+});
+
+
+var runTitle = function(){
+    var string = "Keys Down:";
+    for(var key in keysDown) {
+        var value = Number(key);
+        //string += " " + String.fromCharCode(key);
+    }
+    context.fillStyle = "#ffffff";
+    context.fillText(string, 0, 10, 10000);
+};
+
 var step = function(){
-    update();
-    render();
+    context.fillStyle = '#17111A';
+    context.fillRect(0,0,width,height);
+    
+    switch(gameState){
+        case 0:
+            runTitle();
+            break;
+        case 1:
+            update();
+            render();
+            break;
+    }
     animate(step);
 };
 
@@ -40,8 +84,6 @@ var update = function() {
 };
 
 var render = function(){
-    context.fillStyle = '#17111A';
-  context.fillRect(0,0,width,height);
     context.drawImage(MyIm, 0 + (16 * frame), 0 + (16 * dir), 16, 16, 0, 0, 16, 16);
     
 };
